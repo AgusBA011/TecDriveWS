dc93f70c24145a4edd9061df3088ae66b337d80f
iFreuk <luiguillog2@gmail.com>
2021-11-20 16:31:40 -0600

    Lines Removed
    -Removed iterator import (wasn't used)
    -Remove Simple JSONArray import (wasn't used, till now)

--- TecDriveWS/src/main/java/com/webservice/tecdrivews/resources/JsonHandler.java
+++ TecDriveWS/src/main/java/com/webservice/tecdrivews/resources/JsonHandler.java
@@ -8,8 +8,6 @@ package com.webservice.tecdrivews.resources;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
