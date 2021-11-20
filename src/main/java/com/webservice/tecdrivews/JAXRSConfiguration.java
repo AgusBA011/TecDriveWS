8791be2b491f2617da28c6312f5543eccbd1c338
AgusBA011 <agustinblanco01@gmail.com>
2021-11-20 13:23:41 -0600

    Start

---
+++ src/main/java/com/webservice/tecdrivews/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.webservice.tecdrivews;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
